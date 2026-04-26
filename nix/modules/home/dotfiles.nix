3a246fea63fa33faeb8a01e8191d795c0675c247
Yuya Kurihara <yuya.kurihara@cyberagent.co.jp>
2026-04-27 03:20:43 +0900

    fix: set profile git emails

--- nix/modules/home/dotfiles.nix
+++ nix/modules/home/dotfiles.nix
@@ -7,6 +7,8 @@
     CLAUDE_CONFIG_DIR = "$HOME/.config/claude";
     CLAUDE_CODE_NO_FLICKER = "1";
     GEMINI_CLI_HOME = "$HOME/.config";  # ~/.config/.gemini/ に設定保存
+    # Git
+    GIT_CONFIG_GLOBAL = "$HOME/.config/git/config";
     # pnpm
     PNPM_HOME = "$HOME/.local/share/pnpm";
   } // (if profile ? sshAuthSock then {
